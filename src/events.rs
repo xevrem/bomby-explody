717d018b906dc50d6840819c1b097682bef9066b
Erika Jonell <erika.jonell@gmail.com>
2025-06-06 11:28:32 -0400

    add damage events

--- src/events.rs
+++ src/events.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
     // register events
-    app.add_event::<BlastEvent>();
+    app.add_event::<BlastEvent>().add_event::<DamageEvent>();
 }
 
 #[derive(Event)]
@@ -10,4 +10,10 @@ pub struct BlastEvent {
     pub source: Entity,
     pub location: Vec2,
     pub range: f32,
+}
+
+#[derive(Event)]
+pub struct DamageEvent {
+    pub target: Entity,
+    pub amount: u32,
 }
