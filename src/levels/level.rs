46b31bf54cd2b496db33343fd77724d6e9cd2450
Erika Jonell <erika.jonell@gmail.com>
2025-06-08 12:45:40 -0400

    spawn background on start

--- src/levels/level.rs
+++ src/levels/level.rs
@@ -2,9 +2,13 @@
 
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
+use bevy_prng::WyRand;
+use bevy_rand::global::GlobalEntropy;
+use rand::prelude::*;
 
 use crate::{
     audio::music,
+    background::{create_background, BackgroundAssets},
     components::Level,
     constants::{SCREEN_HEIGHT, SCREEN_WIDTH},
     entities::{
@@ -26,19 +30,25 @@ pub fn spawn_level(
     mut commands: Commands,
     level_assets: Res<LevelAssets>,
     character_assets: Res<CharacterAssets>,
+    background_assets: Res<BackgroundAssets>,
+    mut entropy: GlobalEntropy<WyRand>,
 ) {
-    commands.spawn((
-        Name::new("Level"),
-        Level,
-        Transform::default(),
-        Visibility::default(),
-        StateScoped(Screen::Gameplay),
-        children![
-            create_character(&character_assets,),
-            (
-                Name::new("Gameplay Music"),
-                music(level_assets.music.clone())
-            )
-        ],
-    ));
+    let background = create_background(&mut commands, &background_assets, &mut entropy);
+
+    commands
+        .spawn((
+            Name::new("Level"),
+            Level,
+            Transform::default(),
+            Visibility::default(),
+            StateScoped(Screen::Gameplay),
+            children![
+                create_character(&character_assets,),
+                (
+                    Name::new("Gameplay Music"),
+                    music(level_assets.music.clone())
+                )
+            ],
+        ))
+        .add_child(background);
 }
