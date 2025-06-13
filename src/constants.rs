98eb303eda8ae8d7878c2d0ddf7939505d65d278
Erika Jonell <erika.jonell@gmail.com>
2025-06-13 11:07:52 -0400

    some helpful screen constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,7 @@
 pub const SCREEN_WIDTH: f32 = 1280.0;
 pub const SCREEN_HEIGHT: f32 = 720.0;
+pub const SCREEN_HALF_WIDTH: f32 = 1280.0 / 2.0;
+pub const SCREEN_HALF_HEIGHT: f32 = 720.0 / 2.0;
 
 pub const BASE_TILE_SIZE: f32 = 16.0;
 pub const TILE_SIZE: f32 = BASE_TILE_SIZE * 2.0;
