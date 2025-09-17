21ec338e5e55cd47bcd0293d637c048c0760c512
Erika Jonell <erika.jonell@gmail.com>
2025-09-17 10:05:47 -0400

    add text fade

--- src/screens/wave.rs
+++ src/screens/wave.rs
@@ -29,13 +29,19 @@ fn spawn_wave_screen(mut commands: Commands) {
 }
 
 fn wave_screen_fade(
-    mut query: Single<&mut Countdown, With<WaveScreen>>,
+    mut query: Single<(&mut Countdown, &Children), With<WaveScreen>>,
+    mut child_query: Query<&mut TextColor, With<Text>>,
     mut next_state: ResMut<NextState<WaveState>>,
     time: Res<Time>,
 ) {
-    if query.timer.just_finished() {
+    if query.0.timer.just_finished() {
         next_state.set(WaveState::Running);
     } else {
-        query.timer.tick(time.delta());
+        for mut text_color in child_query.iter_mut() {
+            let mut new_color = text_color.to_srgba();
+            new_color.alpha = query.0.timer.fraction_remaining();
+            text_color.0 = Color::Srgba(new_color);
+        }
+        query.0.timer.tick(time.delta());
     }
 }
