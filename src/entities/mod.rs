5c2a0d5bcf2f2bc1590fc5965ac073841eb08288
Erika Jonell <erika.jonell@gmail.com>
2025-10-05 12:34:34 -0400

    add bullet plugin

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -1,9 +1,10 @@
 use bevy::prelude::*;
 
 pub mod bombs;
+pub mod bullet;
 pub mod enemy;
 pub mod player;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins((bombs::plugin, player::plugin, enemy::plugin));
+    app.add_plugins((bombs::plugin, bullet::plugin, player::plugin, enemy::plugin));
 }
