d99d9248a4d5a02794d8d06729ff82529ce37eb1
Erika Jonell <erika.jonell@gmail.com>
2025-10-13 12:18:14 -0400

    add lob shot to entities module

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -3,8 +3,9 @@ use bevy::prelude::*;
 pub mod bombs;
 pub mod bullet;
 pub mod enemy;
+pub mod lob_shot;
 pub mod player;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins((bombs::plugin, bullet::plugin, player::plugin, enemy::plugin));
+    app.add_plugins((bombs::plugin, bullet::plugin, lob_shot::plugin, player::plugin, enemy::plugin));
 }
