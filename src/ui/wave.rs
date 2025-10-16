42269b1be40b50adc7fb0962bf9e5be23f9c3d48
Erika Jonell <erika.jonell@gmail.com>
2025-10-16 13:25:50 -0400

    create basic wave count ui

--- src/ui/wave.rs
+++ src/ui/wave.rs
@@ -4,10 +4,11 @@
 use bevy::prelude::*;
 
 use crate::{
-    components::{Countdown, Wave},
+    components::{Countdown, Dead, Enemy, EnemyLabel, Health, Wave, WaveLabel},
     screens::Screen,
     theme::prelude::*,
     waves::WaveState,
+    AppSystems, GameplaySystems, PausableSystems,
 };
 
 pub(super) fn plugin(app: &mut App) {
@@ -15,6 +16,14 @@ pub(super) fn plugin(app: &mut App) {
         .add_systems(
             Update,
             wave_ui_fade.run_if(in_state(WaveState::Announce).and(in_state(Screen::Gameplay))),
+        )
+        .add_systems(OnEnter(Screen::Gameplay), create_wave_count_ui)
+        .add_systems(
+            Update,
+            sync_wave_count_ui
+                .in_set(AppSystems::Update)
+                .in_set(PausableSystems)
+                .in_set(GameplaySystems),
         );
 }
 
@@ -59,4 +68,57 @@ fn wave_ui_fade(
         }
         query.0.timer.tick(time.delta());
     }
+}
+
+pub fn create_wave_count_ui(mut commands: Commands) {
+    commands.spawn((
+        Name::new("wave count ui"),
+        Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(10.0),
+            right: Val::Px(10.0),
+            align_items: AlignItems::Start,
+            justify_content: JustifyContent::End,
+            flex_direction: FlexDirection::Column,
+            row_gap: Val::Px(10.0),
+            ..default()
+        },
+        // Don't block picking events for other UI roots.
+        Pickable::IGNORE,
+        GlobalZIndex(2),
+        StateScoped(Screen::Gameplay),
+        children![(
+            Name::new("wave ui grid"),
+            Node {
+                display: Display::Flex,
+                ..default()
+            },
+            children![
+                (
+                    widget::label("Enemies Remaining:"),
+                    Node {
+                        justify_self: JustifySelf::Start,
+                        ..default()
+                    }
+                ),
+                (
+                    widget::label(""),
+                    Node {
+                        justify_self: JustifySelf::End,
+                        ..default()
+                    },
+                    EnemyLabel,
+                    WaveLabel,
+                )
+            ]
+        )],
+    ));
+}
+
+pub fn sync_wave_count_ui(
+    mut label: Single<&mut Text, (With<WaveLabel>, With<EnemyLabel>)>,
+    enemy_query: Query<Entity, (With<Enemy>, With<Health>, Without<Dead>)>,
+) {
+    let val = enemy_query.iter().len();
+    label.0 = format!("{val}");
 }
